821a807ac808541cae875278e39a0217b08b7b49
chris <chris@chriss-MacBook.local>
2019-03-14 20:56:13 -0700

    initial

---
+++ mixtape/src/main/java/scratch/mixtape/io/MixTapeIO.java
@@ -0,0 +1,32 @@
+package scratch.mixtape.io;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import scratch.mixtape.model.Mixtape;
+
+public class MixTapeIO {
+
+	private ObjectMapper mapper;
+
+	public MixTapeIO() {
+		mapper = new ObjectMapper();
+	}
+
+	public Mixtape read(String filename) throws JsonParseException, JsonMappingException, IOException {
+
+		File f = new File(filename);
+		return mapper.readValue(f, Mixtape.class);
+	}
+
+	public void write(String filename, Mixtape mixtape) throws JsonGenerationException, JsonMappingException, IOException {
+		File f = new File(filename);
+
+		mapper.writeValue(f, mixtape);
+	}
+}
---
+++ mixtape/src/main/java/scratch/mixtape/model/User.java
@@ -0,0 +1,9 @@
+package scratch.mixtape.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+	private int id;
+	private String name;
+}
